c5a725e71b0e4e5c478d7cbbe18953f88e4e707e
KunJ99 <kunjeong99@naver.com>
2019-06-26 22:17:12 +0900

    made simple logging tab

---
+++ app/src/main/java/com/example/tapapp/AlarmReceiver.java
@@ -0,0 +1,17 @@
+package com.example.tapapp;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone ringtone = RingtoneManager.getRingtone(context, uri);
+        ringtone.play();
+    }
+}
