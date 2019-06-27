9d7ba1109c6a3791e81bf5aca96259fe32a4c4ab
KunJ99 <kunjeong99@naver.com>
2019-06-27 15:11:19 +0900

    a bit messy but working single alarm setter

--- app/src/main/java/com/example/tapapp/Fragment2.java
+++ app/src/main/java/com/example/tapapp/Fragment2.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Bundle;
@@ -24,6 +26,7 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.view.ViewGroup;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -32,9 +35,17 @@ import static android.content.Context.ALARM_SERVICE;
 
 public class Fragment2 extends Fragment {
     private Calendar c = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"));
-    private SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+    private int timeHour =  c.get(Calendar.HOUR_OF_DAY);
+    private int timeMinute =  c.get(Calendar.MINUTE);
+
+    private SimpleDateFormat df = new SimpleDateFormat("HH:mm");
     private String formattedDate = df.format(c.getTime());
     private TextView textView1;
+    private EditText editText;
+
+    final ArrayList<String> timestamp_list = new ArrayList<>();
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
     public Fragment2() {
 
     }
@@ -51,30 +62,65 @@ public class Fragment2 extends Fragment {
         textView1 = view.findViewById(R.id.msg1);
 
         textView1.setText(formattedDate);
-        final EditText editText = view.findViewById(R.id.input);
+        editText = view.findViewById(R.id.input);
         Button btn = view.findViewById(R.id.button);
         ListView lv = view.findViewById(R.id.lv);
-        final ArrayList<String> timestamp_list = new ArrayList<>();
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, timestamp_list);
         lv.setAdapter(arrayAdapter);
 
+
+        alarmManager = (AlarmManager) getActivity().getSystemService(ALARM_SERVICE);
+        Intent alarmIntent = new Intent(getActivity(), AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(getActivity(), 0, alarmIntent, 0);
+        
         OnClickListener listener = new OnClickListener() {
             public void onClick(View view) {
-                c = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"));
-                formattedDate = df.format(c.getTime());
-//                Bundle bundle = new Bundle();
-//                bundle.putInt(MyConstants.HOUR, timeHour);
-//                bundle.putInt(MyConstants.MINUTE, timeMinute);
-//                bundle.putInt(MyConstants.SECOND, timeSecond);
-//                bundle.putString(MyConstants.NAME, String.valueOf(editText.getText()));
-                timestamp_list.add(formattedDate+ ", " + editText.getText());
-                editText.setText("");
-                arrayAdapter.notifyDataSetChanged();
+//                textView2.setText("");
+                Bundle bundle = new Bundle();
+                bundle.putInt(MyConstants.HOUR, timeHour);
+                bundle.putInt(MyConstants.MINUTE, timeMinute);
+                MyDialogFragment fragment = new MyDialogFragment(new MyHandler());
+                fragment.setArguments(bundle);
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.add(fragment, "time_picker");
+                transaction.commit();
             }
+//                c = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"));
+//                formattedDate = df.format(c.getTime());
+////                Bundle bundle = new Bundle();
+////                bundle.putInt(MyConstants.HOUR, timeHour);
+////                bundle.putInt(MyConstants.MINUTE, timeMinute);
+////                bundle.putInt(MyConstants.SECOND, timeSecond);
+////                bundle.putString(MyConstants.NAME, String.valueOf(editText.getText()));
+//                timestamp_list.add(formattedDate+ ", " + editText.getText());
+//                editText.setText("");
+//                arrayAdapter.notifyDataSetChanged();
         };
         btn.setOnClickListener(listener);
 
         return view;
+    }
+    class MyHandler extends Handler {
+        @Override
+        public void handleMessage (Message msg){
+            Bundle bundle = msg.getData();
+            timeHour = bundle.getInt(MyConstants.HOUR);
+            timeMinute = bundle.getInt(MyConstants.MINUTE);
+            textView1.setText(timeHour + ":" + timeMinute);
+            timestamp_list.add(timeHour + ":" + timeMinute + ", " + editText.getText());
+            setAlarm();
+        }
+    }
+    private void setAlarm(){
+    		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"));
+    		calendar.set(Calendar.HOUR_OF_DAY, timeHour);
+    		calendar.set(Calendar.MINUTE, timeMinute);
+    		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+            } else {
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+            }
     }
 
 }
