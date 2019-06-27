d172ec85117e11782e5a0167e8d91d9eeafe110d
young-rae-kim <dufo7070@kaist.ac.kr>
2019-06-27 19:41:34 +0900

    Setting Activity has been implemented. File deletion problem still happens.

--- app/src/main/java/com/example/tapapp/Fragment1.java
+++ app/src/main/java/com/example/tapapp/Fragment1.java
@@ -5,13 +5,16 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 // import android.widget.GridView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
@@ -20,11 +23,12 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment implements PopupMenu.OnMenuItemClickListener {
 
     GalleryAdapter gallery;
     ImageView imageView;
     ExpandableGridView gridView;
+    private int pos = -1;
     public RequestManager mGlideRequestManager;
     public Fragment1() {
         // Empty Public Constructor
@@ -50,7 +54,7 @@ public class Fragment1 extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 if (null != gallery.getImages() && !gallery.getImages().isEmpty()) {
-                    Snackbar.make(view, gallery.getImages().get(i), Snackbar.LENGTH_SHORT)
+                    Snackbar.make(view, gallery.getImages().get(i), Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                     File imgFile = new File(gallery.getImages().get(i));
                     if (imgFile.exists()) {
@@ -58,7 +62,7 @@ public class Fragment1 extends Fragment {
                         imageView.setImageBitmap(bitmap);
                     }
                     final ImagePopup imagePopup = new ImagePopup(getContext());
-                    imagePopup.setBackgroundColor(Color.argb(100,0,0,0));
+                    imagePopup.setBackgroundColor(Color.argb(128,0,0,0));
                     imagePopup.setFullScreen(true);
                     imagePopup.setHideCloseIcon(true);
                     imagePopup.setImageOnClickClose(true);
@@ -67,6 +71,55 @@ public class Fragment1 extends Fragment {
                 }
             }
         });
+
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (null != gallery.getImages() && !gallery.getImages().isEmpty()) {
+                    showPopup(view, i);
+                }
+                return true;
+            }
+        });
+
         return view;
     }
+
+    public void showPopup(View v, int i) {
+        PopupMenu popup = new PopupMenu(getContext(), v);
+        this.pos = i;
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.menu_popup);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.delete:
+                // should implement
+                if (this.pos >= 0) {
+                    File imgFile = new File(gallery.getImages().get(this.pos));
+                    if (imgFile.exists()) {
+                        if (!imgFile.delete()) {
+                            Snackbar.make(getView(), "Deletion failed.", Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                        } else {
+                            Snackbar.make(getView(), "Deleted successfully.", Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                        }
+                    }
+                    this.pos = -1;
+                }
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                transaction.replace(R.id.fragment, this);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                return true;
+             default:
+                return true;
+        }
+    }
+
 }
