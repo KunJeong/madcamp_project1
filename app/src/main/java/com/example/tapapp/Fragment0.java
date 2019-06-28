b50e1467a1a943099593202ffbe2876167a1ce42
lynn312 <lynn312@unist.ac.kr>
2019-06-28 16:17:57 +0900

    Finally Merged.

--- app/src/main/java/com/example/tapapp/Fragment0.java
+++ app/src/main/java/com/example/tapapp/Fragment0.java
@@ -1,22 +1,47 @@
 package com.example.tapapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment0 extends Fragment {
-
+    private final int REQUEST_CODE = 99;
     public Fragment0() {
         // Empty Public Constructor
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_0, container, false);
+        View view = inflater.inflate(R.layout.fragment_0, container, false);
+        Button btPick = (Button) view.findViewById(R.id.btpick_contact);
+        btPick.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI); // 휴대폰 정보 가져오기 위해
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+        return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
 }
